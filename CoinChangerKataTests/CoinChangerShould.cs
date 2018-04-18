1ce44783c2d8d62d63dbc9a86517608e097a2b5a
Abe Lourenco <alourenco@vistaprint.com>
2018-04-18 13:58:32 +0200

    Refactor

--- CoinChangerKataTests/CoinChangerShould.cs
+++ CoinChangerKataTests/CoinChangerShould.cs
@@ -8,36 +8,15 @@ namespace CoinChangerKataTests
     {
         private readonly CoinChanger _coinChanger = new CoinChanger();
 
-        [Test]
-        public void Return_1_cent_when_change_is_1_cent_and_coin_is_1_cent()
+        [TestCase(1, new[] { 1 }, new[] { 1 })]
+        [TestCase(2, new[] { 1 }, new[] { 2 })]
+        [TestCase(5, new[] { 1, 5 }, new[] { 0, 1 })]
+        [TestCase(6, new[] { 1, 5 }, new[] { 1, 1 })]
+        public void Return_correct_change(int change, int[] availableCoins, int[] expectedChangeCoins)
         {
-            int[] change = _coinChanger.Change(1, new int[] { 1 });
+            int[] changeCoins = _coinChanger.Change(change, availableCoins);
 
-            Assert.AreEqual(new int[] {1}, change);
-        }
-
-        [Test]
-        public void Return_2_cents_coins_when_change_is_2_cents_and_coin_is_1_cent()
-        {
-            int[] change = _coinChanger.Change(2, new int[] { 1 });
-
-            Assert.AreEqual(new int[] {2}, change);
-        }
-
-        [Test]
-        public void Return_5_cent_coin_when_change_is_5_cents_and_coin_is_1_5_cents()
-        {
-            int[] change = _coinChanger.Change(5, new int[] { 1, 5 });
-
-            Assert.AreEqual(new int[] { 0, 1 }, change);
-        }
-
-        [Test]
-        public void Return_1_5_cent_coin_when_change_is_6_cents_and_coin_is_1_5_cents()
-        {
-            int[] change = _coinChanger.Change(6, new int[] { 1, 5 });
-
-            Assert.AreEqual(new int[] { 1, 1 }, change);
+            Assert.AreEqual(expectedChangeCoins, changeCoins);
         }
     }
 }
